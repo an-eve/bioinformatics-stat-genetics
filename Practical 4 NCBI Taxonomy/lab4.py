2cd64bf0381f6c8aaa77eaed84e9cda515ac6249
Anna Putina <102035723+an-eve@users.noreply.github.com>
2023-10-16 20:03:46 +0300

    Everything except Kingdom question

--- Practical 4 NCBI Taxonomy/lab4.py
+++ Practical 4 NCBI Taxonomy/lab4.py
@@ -2,90 +2,128 @@ import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = "nodes.dmp"
+# Test the code on a smaller tree
 
+# Read the data
+file_path = "test.dmp"
 df = pd.read_csv(file_path, delimiter = r"\s+\|\s+", usecols=[0, 1, 2], engine = "python", header=None, names = ["child", "parent", "rank"]) 
-df.head()
+print('Test small tree: \n', df.head())
 
+# Add ranks
 attrs = dict(zip(df['child'], df['rank']))
 
-
-# Find rows where values in the first and second columns are the same
-duplicate_rows = df[df['parent'] == df['child']]
-
-# Delete the duplicate rows by index
-df = df.drop(duplicate_rows.index)
-
-# Reset the index if needed
-df = df.reset_index(drop=True)
-
+# Create the graph
 G = nx.from_pandas_edgelist(df, source='parent', target='child', create_using=nx.DiGraph())
-
 nx.set_node_attributes(G, attrs, name="rank")
 
-list(nx.nodes_with_selfloops(G))
+# Check self-loops and remove these edges
+print('Nodes with self-loops: ', list(nx.nodes_with_selfloops(G)))
 G.remove_edge(1,1)
 
-nx.is_tree(G)
-nx.is_directed(G)
-nx.is_arborescence(G)
-nx.is_directed_acyclic_graph(G)
+# Type of the graph
+print('Is it a tree? ', nx.is_tree(G))
+print('Is it directed? ', nx.is_directed(G))
+print('Is it rooted? ', nx.is_arborescence(G))
+print('Is it a DAG? ', nx.is_directed_acyclic_graph(G))
 
-G.number_of_nodes()
+# The number of nodes
+print('The number of nodes: ', G.number_of_nodes())
 
-valid_ranks = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
-restricted_nodes = [node for node in G.nodes() if not G.nodes[node]['rank'] in valid_ranks]
-
-G.edges()
-G.nodes(data=True)
-
-G.nodes[1]["rank"] = "rank"
-
-not_tree_nodes = [node for node, attrs in G.nodes(data=True) if attrs['rank'] != 'rank']
-restricted_tree_nodes = [node for node, attrs in G.nodes(data=True) if attrs['rank'] == 'rank']
-restricted_tree = nx.subgraph(restricted_tree, restricted_tree_nodes)
-
-not_tree_nodes = [node for node, attrs in G.nodes(data=True) if not attrs['rank'] in valid_ranks]
-restricted_tree_nodes = [node for node, attrs in G.nodes(data=True) if attrs['rank'] in valid_ranks]
-restricted_tree = nx.subgraph(restricted_tree, restricted_tree_nodes)
-
-restricted_tree.nodes(data=True)
-
-[attrs for node, attrs in G.nodes(data=True)]
-
-"""
-# Draw the directed graph
+# Draw the tree
 pos = nx.spring_layout(G)
 nx.draw_networkx(G, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
 plt.title("Rooted Tree with Ranks")
 plt.show()
-"""
 
-"""
-# Draw the directed graph
-pos = nx.spring_layout(restricted_tree)
-nx.draw_networkx(restricted_tree, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
-plt.title("Rooted Tree with Ranks")
-plt.show()
-"""
-ls = [list(G.successors(i)) for i in not_tree_nodes]
-lp = [list(G.predecessors(i)) for i in not_tree_nodes]
-list(restricted_tree.predecessors(2))
 
-restricted_tree = G.copy()
+# Nodes to remove
+not_nodes = [node for node, attrs in G.nodes(data=True) if attrs['rank'] != 'rank']
+
+# Nodes to remove
+restricted_nodes = [node for node, attrs in G.nodes(data=True) if attrs['rank'] == 'rank']
+
+restricted_taxonomy = G.copy()
+
+#Lists of successors and predecessors for the nodes wich will be removed
+list_succes = [list(G.successors(i)) for i in not_nodes]
+list_pred = [list(G.predecessors(i)) for i in not_nodes]
+
 # Add edges from the destination node to the children
-restricted_tree.add_edges_from([(lp[i][0], ls[i][j]) for i in range(1,len(ls)) for j in range(len(ls[i]))])
+restricted_taxonomy.add_edges_from([(list_pred[i][0], list_succes[i][j]) for i in range(1,len(list_succes)) for j in range(len(list_succes[i]))])
 
-# Remove edges from the source node to the children
-restricted_tree.remove_edges_from([(source_node, child) for child in children])
+# Restrict
+restricted_taxonomy = nx.subgraph(restricted_taxonomy, restricted_nodes)
 
-restricted_tree.number_of_nodes()
 
-nx.is_tree(restricted_tree)
-nx.is_arborescence(restricted_tree)
+# Draw the restricted graph
+pos = nx.spring_layout(restricted_taxonomy)
+nx.draw_networkx(restricted_taxonomy, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
+plt.title("Restricted graph")
+plt.show()
 
-[node for node in restricted_tree.nodes() if restricted_tree.in_degree(node) == 0]
 
-restricted_tree.nodes[1090]["rank"]
+#--------------------------------------------------------------------------------------
 
-ls
+# Work with the whole dataset
+
+# Read the data
+file_path = "nodes.dmp"
+df = pd.read_csv(file_path, delimiter = r"\s+\|\s+", usecols=[0, 1, 2], engine = "python", header=None, names = ["child", "parent", "rank"]) 
+print('Test small tree: \n', df.head())
+
+# Add ranks
+attrs = dict(zip(df['child'], df['rank']))
+
+# Create the graph
+G = nx.from_pandas_edgelist(df, source='parent', target='child', create_using=nx.DiGraph())
+nx.set_node_attributes(G, attrs, name="rank")
+
+"""
+list(G.successors(10239))
+G.nodes[2840056]["rank"]
+"""
+
+# Check self-loops and remove these edges
+print('Nodes with self-loops: ', list(nx.nodes_with_selfloops(G)))
+G.remove_edge(1,1)
+
+# Type of the graph
+print('Is it a tree? ', nx.is_tree(G))
+print('Is it directed? ', nx.is_directed(G))
+print('Is it rooted? ', nx.is_arborescence(G))
+print('Is it a DAG? ', nx.is_directed_acyclic_graph(G))
+
+# The number of nodes
+print('The number of nodes: ', G.number_of_nodes())
+
+valid_ranks = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
+
+# Nodes to remove
+not_nodes = [node for node, attrs in G.nodes(data=True) if not attrs['rank'] in valid_ranks]
+
+# Nodes to remove
+restricted_nodes = [node for node, attrs in G.nodes(data=True) if attrs['rank'] in valid_ranks]
+
+restricted_taxonomy = G.copy()
+
+# Remove the info about the node 1 from these lists because it's a root
+not_nodes[0]
+not_nodes = not_nodes[1:]
+
+#Lists of successors and predecessors for the nodes wich will be removed
+list_succes = [list(G.successors(i)) for i in not_nodes]
+list_pred = [list(G.predecessors(i)) for i in not_nodes]
+
+# Add edges from the destination node to the children
+restricted_taxonomy.add_edges_from([(list_pred[i][0], list_succes[i][j]) for i in range(1,len(list_succes)) for j in range(len(list_succes[i]))])
+
+# Restrict
+restricted_taxonomy = nx.subgraph(restricted_taxonomy, restricted_nodes)
+
+# The number of nodes
+print('The number of nodes in the restricted version: ', restricted_taxonomy.number_of_nodes())
+
+# The nodes with in-degree 0 and their ranks
+search_top = [(node, restricted_taxonomy.nodes[node]["rank"])  for node in restricted_taxonomy.nodes() if restricted_taxonomy.in_degree(node) == 0]
+search_top_list = [x[1] for x in search_top]
+unique_elements = [x for i, x in enumerate(search_top_list) if x not in search_top_list[:i]]
